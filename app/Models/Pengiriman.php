80709e98e20258390f03eb1502b59c8042b7b81a
wirabudhi <bpantaiselatan@gmail.com>
2025-01-19 23:43:38 +0800

    Add Pembayaran model, factory, migration, and seeder; introduce Pengiriman model

--- app/Models/Pengiriman.php
+++ app/Models/Pengiriman.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Pembayaran extends Model
+class Pengiriman extends Model
 {
-    /** @use HasFactory<\Database\Factories\PembayaranFactory> */
+    /** @use HasFactory<\Database\Factories\PengirimanFactory> */
     use HasFactory;
 }
